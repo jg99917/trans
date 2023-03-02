855e0d5fc5e4d440f1ebec89e3c205a49c027f45
shin1357 <jg99917@naver.com>
2023-03-03 02:48:07 +0900

    day1

---
+++ naver.py
@@ -0,0 +1,21 @@
+import os
+import sys
+import urllib.request
+import json
+
+client_id = "0xdYrf7TC9vSl235FR88" # 개발자센터에서 발급받은 Client ID 값
+client_secret = "Vu083a8VAh" # 개발자센터에서 발급받은 Client Secret 값
+encText = urllib.parse.quote("教え て くだ さい ね お")
+data = "source=ja&target=ko&text=" + encText
+url = "https://openapi.naver.com/v1/papago/n2mt"
+request = urllib.request.Request(url)
+request.add_header("X-Naver-Client-Id",client_id)
+request.add_header("X-Naver-Client-Secret",client_secret)
+response = urllib.request.urlopen(request, data=data.encode("utf-8"))
+rescode = response.getcode()
+if(rescode==200):
+    response_body = response.read().decode('utf-8')
+    result = json.loads(response_body)['message']['result']
+    print(result['translatedText'])
+else:
+    print("Error Code:" + rescode)
