bbe055b9ae291ca521509d4503b0980c9e44438a
shin1357 <jg99917@naver.com>
2023-03-03 02:52:00 +0900

    day1 final

--- naver.py
+++ naver.py
@@ -1,11 +1,15 @@
-import os
-import sys
 import urllib.request
 import json
+import pytesseract as pyte
+
+pyte.pytesseract.tesseract_cmd = r'C:/Program Files/Tesseract-OCR/tesseract'
+
+a = pyte.image_to_string('3.PNG', lang='jpn_vert', config='--psm 5 --oem 1')
+
 
 client_id = "0xdYrf7TC9vSl235FR88" # 개발자센터에서 발급받은 Client ID 값
 client_secret = "Vu083a8VAh" # 개발자센터에서 발급받은 Client Secret 값
-encText = urllib.parse.quote("教え て くだ さい ね お")
+encText = urllib.parse.quote(a)
 data = "source=ja&target=ko&text=" + encText
 url = "https://openapi.naver.com/v1/papago/n2mt"
 request = urllib.request.Request(url)
